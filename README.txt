0b20431a2d7368392eccf3569156368a6c30c492
Tarun Kumar <39281436+kumatrx@users.noreply.github.com>
2021-08-04 16:17:38 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+(Running)
+LoadBalancer URL - http://udagr-webap-17ibaubu0fs23-855948072.us-west-2.elb.amazonaws.com/
---
+++ deployment.sh
@@ -0,0 +1,2 @@
+./create.sh UdagramStack1 network.yml network-param.json    
+./create.sh UdagramStack2 servers.yml server-param.json
